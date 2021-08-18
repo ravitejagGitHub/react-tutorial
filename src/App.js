ebbbccc51eee9ff99ad931c143daa477a1d1c4d9
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-08-18 12:10:12 +0530

    props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import { Table } from "./Table";
 
 export class App extends Component {
     render() {
+        const characters = [
+            {
+              name: 'Raviteja',
+              job: 'Software',
+            },
+            {
+              name: 'Chakri',
+              job: 'Loco pilot',
+            },
+            {
+              name: 'Sravani',
+              job: 'House wife',
+            }
+          ]
         return (
             <div className="app">
                 <h1> Hello Raviteja, Welcome to React.</h1>
-                <Table/>
+                <Table charactersData={ characters }/>
             </div>
         );
     }
--- src/Table.js
+++ src/Table.js
@@ -11,25 +11,18 @@ const TableHeader = () => {
     );
 }
 
-const TableBody = () => {
+const TableBody = (props) => {
+    const rows = props.charactersData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
     return (
         <tbody>
-            <tr>
-                <td>Charlie</td>
-                <td>Janitor</td>
-            </tr>
-            <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-            </tr>
-            <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
+            {rows}
         </tbody>
     );
 }
@@ -37,10 +30,11 @@ const TableBody = () => {
 
 export class Table extends Component {
     render() {
+        const { charactersData } = this.props;
       return (
         <table>
           <TableHeader />
-          <TableBody />
+              <TableBody charactersData={ charactersData}/>
         </table>
       )
     }
