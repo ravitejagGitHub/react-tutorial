a15e996ff3ed4fadfaf1df16278785eb24c40d4e
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-08-24 22:58:31 +0530

    PreventingRendering still run life cycle methods

--- src/ConditionalRendering/PreventingRendering/WarningBanner.js
+++ src/ConditionalRendering/PreventingRendering/WarningBanner.js
@@ -1,11 +1,18 @@
-export function WarningBanner(props) {
-    if (!props.warn) {
-      return null;
-    }
-  
-    return (
-      <div className="warning">
-        Warning!
-      </div>
-    );
-  }
+import { Component } from "react";
+
+export class WarningBanner extends Component {
+	componentDidUpdate() {
+		console.log("---componentDidUpdate---");
+	}
+
+	componentDidMount() {
+		console.log("---componentDidMount---");
+	}
+	render() {
+		if (!this.props.warn) {
+			return null;
+		}
+
+		return <div className="warning">Warning!</div>;
+	}
+}
