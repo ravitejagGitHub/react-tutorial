95e6faa1b990f8504e61096f9567640796d54935
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-08-24 23:20:58 +0530

    list keys dont use index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 // import { App } from './App';
-import { Clock } from './State/Clock';
+import { Clock } from "./State/Clock";
 
-import './index.css';
-import { Counter } from './State/Counter';
+import "./index.css";
+import { Counter } from "./State/Counter";
 
-import { Toggle } from './HandlingEvents/Toggle';
-import { LoginControl } from './ConditionalRendering/LoginControl';
-import { Page } from './ConditionalRendering/PreventingRendering/Page';
-
+import { Toggle } from "./HandlingEvents/Toggle";
+import { LoginControl } from "./ConditionalRendering/LoginControl";
+import { Page } from "./ConditionalRendering/PreventingRendering/Page";
+import { NumberList } from "./ListsAndKeys/List";
 
 ReactDOM.render(
-    <>
-        <Clock />
-        <Counter />
-        <Toggle />
-        <LoginControl />
-        <Page />
-    </>
-    , document.querySelector('#root'))
-
+	<>
+		<Clock />
+		<Counter />
+		<Toggle />
+		<LoginControl />
+		<Page />
+		<NumberList numbers={[1, 2, 3, 4, 5, 6]} />
+	</>,
+	document.querySelector("#root")
+);
