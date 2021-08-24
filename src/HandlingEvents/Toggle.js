2a92d7fcbe465a391749d0c3a78535198fc95965
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-08-24 21:03:03 +0530

    handle event with bind

---
+++ src/HandlingEvents/Toggle.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+
+export class Toggle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: false };
+
+        this.togglesState = this.togglesState.bind(this);
+    }
+
+    togglesState() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.togglesState}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Clock } from './State/Clock';
 import './index.css';
 import { Counter } from './State/Counter';
 
+import { Toggle } from './HandlingEvents/Toggle';
 
 
 ReactDOM.render(
     <>
         <Clock />
         <Counter />
+        <Toggle />
     </>
     , document.querySelector('#root'))
 
